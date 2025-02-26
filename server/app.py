1ba082b2c17e5e09645d6237a0ec2558c73c7b1a
sai7-12 <saiteja-intern@kaditinnovations.com>
2025-02-26 08:23:58 -0700

    Update app.py

--- server/app.py
+++ server/app.py
@@ -90,5 +90,5 @@ def chat_endpoint():
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=5000, debug=True)
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
